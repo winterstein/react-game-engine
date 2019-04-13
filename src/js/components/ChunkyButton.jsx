9473517a0c149ea3e4e578d074ceebc74ec2d2db
Daniel Winterstein <daniel@sodash.com>
2019-04-13 15:59:56 +0100

    tweaks

--- src/js/components/ChunkyButton.jsx
+++ src/js/components/ChunkyButton.jsx
@@ -8,16 +8,16 @@ const ChunkyButton = ({player, onClick}) => {
 	let style = {
 		position: 'fixed',
 		zIndex:10000,
-		width: '5vw',
-		height: '5vw',
-		borderRadius: '2.5vw',
+		width: '10vw',
+		height: '10vw',
+		borderRadius: '5vw',
 	};
 	if (pi) {
-		style.bottom = '10px';
-		style.right = '10px';
+		style.bottom = '-1vw';
+		style.right = '-1vw';
 	} else {
-		style.top = '10px';
-		style.left = '10px';
+		style.top = '-1vw';
+		style.left = '-1vw';
 	}
 	return (
 <button style={style} className='btn btn-primary btn-xlg' onClick={onClick}> {pi} </button>
