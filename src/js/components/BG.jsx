904f7fd9a6d301fd832a9196328bfd24d4533e86
Daniel Winterstein <daniel@good-loop.com>
2020-05-20 11:35:05 +0100

    bg

---
+++ src/js/components/BG.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+const BG = ({src, children, opacity=0.5}) => {
+	let style= {
+		background: `url('${src}')`,
+		backgroundSize: 'cover',
+		height:"100%", width:"100%",
+		position:'fixed',
+		top:0, left:0,right:0,bottom:0,
+		zIndex:0,
+		opacity
+	};
+	return (<div >
+		<div style={style} />
+		{children}
+	</div>);
+};
+export default BG;
