dc90800b7b296ef811f4f3552599b723736a130d
Daniel Winterstein <daniel@good-loop.com>
2020-06-29 22:37:17 +0100

    CI...

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,6 +4,7 @@ console.log(config)
 module.exports = {
 	launch: {
 		headless: config.head,
+        slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
 		executablePath: config.chrome ? '/usr/bin/google-chrome-stable' : ''
 	}
 };
