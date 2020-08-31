2e7a66cd846df826e9b43bb261ed91b0ff244134
Daniel Winterstein <daniel@good-loop.com>
2020-08-31 11:36:53 +0100

    DataClass fix + PropControl tweaks

--- src/js/components/StoryPage.jsx
+++ src/js/components/StoryPage.jsx
@@ -42,24 +42,52 @@ let ticker = new StopWatch({tickLength:700});
 const spaceKey = new Key(" ");
 
 class StoryTree {
+	/**
+	 * @type {Tree}
+	 */
+	history;
+
 	constructor(text) {
 		this.text = text;
-		this.root = new Tree();
+		this.root = new Tree({value:"root"});
+		this.history = new Tree();		
+		this.tSentence4id = {};
 		let sections = text.split(/^##\s/m);
 		sections.forEach((section,i) => {
 			let tSection = Tree.add(this.root, {index:i});
 			let scenes = section.split(/^###\s/m);
 			scenes.forEach((scene,j) => {
 				let tScene = Tree.add(tSection, {index:j});
-				let scenes = section.split(/^###\s/m);
+				let sentences = scene.split(/^\n *\n/m);
+				sentences.forEach((sentence,k) => {
+					let tSentence = Tree.add(tScene, {index:k,id:nonce(),text:sentence.trim()});
+					this.tSentence4id[tSentence.value.id] = tSentence;
+				});
 			});
 		});
+		Tree.add(this.history, this.root.value);
 	}
 };
+StoryTree.next = storyTree => {
+	const a = {};
+	// what is the latest value?
+	Tree.map(storyTree.history, node => {
+		a.latest = node.value;
+	});
+	// what comes next? find latest then step on
+	Tree.map(storyTree.root, node => {
+		if (a.latestFound && ! a.next) {
+			a.next = node;
+		}
+	});
+	return a.next;
+};
+
+const Emoji = ({children}) => <span aria-label='emoji' role='img'>{children}</span>;
 
 const StoryPage = () => {
-
-	let pvChapter = DataStore.fetch(['misc','chapter',1], () => {
+	const chapterNum = 1;
+	let pvChapter = DataStore.fetch(['misc','chapter',chapterNum], () => {
 		return ServerIO.load("/data/book/chapter1.md");
 	});
 	if ( ! pvChapter.value) return <Misc.Loading/>;
@@ -68,34 +96,42 @@ const StoryPage = () => {
 	let _title = chapter.match(/^# (.+)$/m);
 	let title = (_title && _title[1]) || "";
 	
+	const storyTree = DataStore.getValue(['misc','StoryTree',chapterNum]) || DataStore.setValue(['misc','StoryTree',chapterNum], new StoryTree(chapter));	
 	let bookmark = DataStore.getUrlValue('bookmark') || DataStore.setUrlValue('bookmark', "", false);
+
+
 	setTimeout(() => DataStore.update(), 500);
-	let st = new StoryTree(chapter);
-	console.log(st);
+	console.log(storyTree);
 
 	return (<div className='open-book container'>
 		<BG src='/img/src/bg/open-book.jpg' size='fit' opacity={1}>
-			<div className='right-page'>
-				{JSON.stringify(st)}
+			<div className='right-page'>				
+				{Tree.map(storyTree.history, t => <div>{t.value.index+" "+t.value.text}</div>)}
+				<hr/>
+								
+				<Button color='primary' onClick={e => StoryTree.next(storyTree)} ><Emoji>✏️</Emoji> ... </Button>
+
+				<hr/>
+				{JSON.stringify(storyTree)}
 			</div>
 		</BG>
 	</div>);
 };
 
-const Section = ({section, sectionIndex, bookmark}) => {
-	let scenes = Bookmark.scenes(section);
-	return <div>{scenes.map((s,j) => <Scene key={j} scene={s} sectionIndex={sectionIndex} sceneIndex={j} bookmark={bookmark} />)}<hr/></div>;
-};
+// const Section = ({section, sectionIndex, bookmark}) => {
+// 	let scenes = Bookmark.scenes(section);
+// 	return <div>{scenes.map((s,j) => <Scene key={j} scene={s} sectionIndex={sectionIndex} sceneIndex={j} bookmark={bookmark} />)}<hr/></div>;
+// };
 
 
-const Scene = ({scene, sectionIndex, sceneIndex, bookmark}) => {
-	let sentences = Bookmark.sentences(scene);
-	return <div>{sentences.map((s,i) => <Sentence key={i} sentenceIndex={i} text={s} sceneIndex={sceneIndex} sectionIndex={sectionIndex} bookmark={bookmark} />)}</div>;
-};
+// const Scene = ({scene, sectionIndex, sceneIndex, bookmark}) => {
+// 	let sentences = Bookmark.sentences(scene);
+// 	return <div>{sentences.map((s,i) => <Sentence key={i} sentenceIndex={i} text={s} sceneIndex={sceneIndex} sectionIndex={sectionIndex} bookmark={bookmark} />)}</div>;
+// };
 
-const Sentence = ({text, sentenceIndex, sceneIndex, sectionIndex, bookmark}) => {
-	let isLatest = [sectionIndex, sceneIndex, sentenceIndex].join('.') === bookmark;
-	return <div>{sectionIndex}.{sceneIndex}.{sentenceIndex}: <MDText source={text} /> {isLatest}</div>;
-};
+// const Sentence = ({text, sentenceIndex, sceneIndex, sectionIndex, bookmark}) => {
+// 	let isLatest = [sectionIndex, sceneIndex, sentenceIndex].join('.') === bookmark;
+// 	return <div>{sectionIndex}.{sceneIndex}.{sentenceIndex}: <MDText source={text} /> {isLatest}</div>;
+// };
 
 export default StoryPage;
