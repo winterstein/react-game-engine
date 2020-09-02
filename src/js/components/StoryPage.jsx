b70e9be0cdb9783ae3bd238360e6ce9909f16b3f
Daniel Winterstein <daniel@good-loop.com>
2020-09-02 08:36:53 +0100

    it-works

--- src/js/components/StoryPage.jsx
+++ src/js/components/StoryPage.jsx
@@ -43,7 +43,7 @@ const spaceKey = new Key(" ");
 
 class StoryTree {
 	/**
-	 * @type {Tree}
+	 * @type {Tree} Never null
 	 */
 	history;
 
@@ -54,13 +54,15 @@ class StoryTree {
 		this.tSentence4id = {};
 		let sections = text.split(/^##\s/m);
 		sections.forEach((section,i) => {
-			let tSection = Tree.add(this.root, {index:i});
+			let tSection = Tree.add(this.root, {index:""+i});
 			let scenes = section.split(/^###\s/m);
 			scenes.forEach((scene,j) => {
-				let tScene = Tree.add(tSection, {index:j});
-				let sentences = scene.split(/^\n *\n/m);
+				let tScene = Tree.add(tSection, {index:i+"."+j});
+				let sentences = scene.split(/\n *\n/);
 				sentences.forEach((sentence,k) => {
-					let tSentence = Tree.add(tScene, {index:k,id:nonce(),text:sentence.trim()});
+					sentence = sentence.trim();
+					if ( ! sentence) return; // skip blank lines
+					let tSentence = Tree.add(tScene, {index:i+"."+j+"."+k, id:nonce(),text:sentence});
 					this.tSentence4id[tSentence.value.id] = tSentence;
 				});
 			});
@@ -68,19 +70,32 @@ class StoryTree {
 		Tree.add(this.history, this.root.value);
 	}
 };
+/**
+ * 
+ * @param {StoryTree} storyTree 
+ * @returns {Tree}
+ */
 StoryTree.next = storyTree => {
-	const a = {};
-	// what is the latest value?
-	Tree.map(storyTree.history, node => {
-		a.latest = node.value;
-	});
 	// what comes next? find latest then step on
-	Tree.map(storyTree.root, node => {
-		if (a.latestFound && ! a.next) {
-			a.next = node;
+	let olds = Tree.flatten(storyTree.history);
+	let last = olds[olds.length-1]; 
+	Tree.assIsa(last);
+	let nodes = Tree.flatten(storyTree.root);
+	let nextNode;
+	for(let i=0; i<nodes.length; i++) {
+		if (nodes[i].value === last.value) {
+			nextNode = nodes[i+1];
+			break;
 		}
-	});
-	return a.next;
+	}
+	if ( ! nextNode) {
+		// all done
+		return null;
+	}
+	// add to history
+	Tree.add(storyTree.history, nextNode.value);
+	// return
+	return nextNode;
 };
 
 const Emoji = ({children}) => <span aria-label='emoji' role='img'>{children}</span>;
@@ -96,26 +111,27 @@ const StoryPage = () => {
 	let _title = chapter.match(/^# (.+)$/m);
 	let title = (_title && _title[1]) || "";
 	
-	const storyTree = DataStore.getValue(['misc','StoryTree',chapterNum]) || DataStore.setValue(['misc','StoryTree',chapterNum], new StoryTree(chapter));	
+	const storyTree = DataStore.getValue(['misc','StoryTree',chapterNum]) || DataStore.setValue(['misc','StoryTree',chapterNum], new StoryTree(chapter), false);
 	let bookmark = DataStore.getUrlValue('bookmark') || DataStore.setUrlValue('bookmark', "", false);
 
 
-	setTimeout(() => DataStore.update(), 500);
-	console.log(storyTree);
+	setTimeout(() => DataStore.update(), 500);	
 
 	return (<div className='open-book container'>
 		<BG src='/img/src/bg/open-book.jpg' size='fit' opacity={1}>
 			<div className='right-page'>				
-				{Tree.flatten(storyTree.history).map(t => <div>{t.value? t.value.index+" "+t.value.text : "-"}</div>)}
+				{Tree.flatten(storyTree.history).map((t,i) => <StoryLine key={i} node={t} />)}
 				<hr/>
 								
 				<Button color='primary' onClick={e => StoryTree.next(storyTree)} ><Emoji>✏️</Emoji> ... </Button>
 
-				<hr/>
-				{JSON.stringify(storyTree)}
 			</div>
 		</BG>
 	</div>);
+};
+
+const StoryLine = ({node}) => {
+	return <div key={i}>{node.value? <MDText source={node.value.text || ""+node.value.index} /> : "-"}</div>;
 };
 
 // const Section = ({section, sectionIndex, bookmark}) => {
